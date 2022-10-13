267cf0bb31f8a707f7940bc1787949330287c201
khalid <khalidsaifullah095@gmail.com>
2022-10-13 17:47:33 +0800

    demo

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+$('.team_part').slick({
+            
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          
+            prevArrow:false,
+            nextArrow:false,
+            dots:true,
+           
+            // prevArrow: ".banner_prev",
+            // nextArrow: ".banner_next",
+        
+    });
+    $('.customer_part').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow:false,
+      nextArrow:false,
+      dots:true,
+     
+      // prevArrow: ".banner_prev",
+      // nextArrow: ".banner_next",
+  
+});
+
+var mixer = mixitup('.abc');
+
+    
